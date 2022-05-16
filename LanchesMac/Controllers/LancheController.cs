ccd32d50a360cae1af9a70d6f8fb37b0371e5dbb
Danilo Lima <danilo.lima@csjsistemas.com.br>
2022-05-16 17:56:54 -0300

    acrescentando partial view

--- LanchesMac/Controllers/LancheController.cs
+++ LanchesMac/Controllers/LancheController.cs
@@ -1,4 +1,5 @@
 ﻿using LanchesMac.Repositories.Interfaces;
+using LanchesMac.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LanchesMac.Controllers
@@ -14,8 +15,14 @@ namespace LanchesMac.Controllers
 
         public IActionResult Lista()
         {
-            var lanches = _lancheRepository.Lanches;
-            return View(lanches);
+
+            //var lanches = _lancheRepository.Lanches;
+            //return View(lanches);
+            var LanchesListViewModel = new LancheListViewModel();
+            LanchesListViewModel.Lanches = _lancheRepository.Lanches;
+            LanchesListViewModel.CategoriaAtual = "Categoria Atual";
+
+            return View(LanchesListViewModel);
         }
     }
 }
